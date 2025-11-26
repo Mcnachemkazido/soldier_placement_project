aa4fb5de5df9b68178ed5dd6b4aec9cb35803367
MenachemKazido <agbg7172@gmail.com>
2025-11-26 12:57:38 +0200

    A revised version of Management

--- models/management.py
+++ models/management.py
@@ -7,7 +7,7 @@ class Management:
     def __init__(self,list_of_soldiers):
         self.list_of_soldiers = list_of_soldiers
 
-    def to_fill_a_first_house(self):
+    def fill_the_houses(self):
         full_houses = []
         houses = ["Dorm_A" ,"Dorm_B"]
         for h in houses:
@@ -15,20 +15,17 @@ class Management:
             room_count = 1
             while not (house.is_full()) and self.list_of_soldiers:
                 room = Room(room_count)
-                house.add_room(room)
                 room_count += 1
                 while not(room.is_full()) and self.list_of_soldiers:
                     room.add_bed(self.list_of_soldiers.pop())
+                house.add_room(room)
             full_houses.append(house)
         return full_houses , self.list_of_soldiers
 
 
+s2 = Soldier(1,2,4,56,5)
+s1 = Soldier(1,2,4,56,5)
+m= Management([s1,s2])
+f = m.fill_the_houses()
+print(f[0][0].is_full())
 
-x = [i for i in range(1000)]
-m = Management(x)
-h = m.to_fill_a_first_house()
-print(h)
-for i in h[0]:
-    x = i.rooms
-    for j in x:
-        print(j.sleeping_places)
